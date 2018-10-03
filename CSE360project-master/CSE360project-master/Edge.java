53964859fa33c4036eedb8109d0b2eb3893e9ae7
elin620 <43224346+elin620@users.noreply.github.com>
2018-10-03 14:31:10 -0700

    Add files via upload

---
+++ CSE360project-master/CSE360project-master/Edge.java
@@ -0,0 +1,53 @@
+public class Edge {
+	
+	private String edgeHead;
+	private String edgeTail;
+	private int tailDuration;
+	
+	// Constructor
+    public Edge() {
+        edgeHead = "";
+        edgeTail = "";
+        tailDuration = 0;
+    }
+    
+    //Access 
+    
+    public String getEdgeHead() {
+        return edgeHead;
+    }
+    
+    public String getEdgeTail() {
+        return edgeTail;
+    }
+    
+    public int getTailDuration() {
+        return tailDuration;
+    }
+    
+    //Set
+    
+    public void setEdgeHead(String name) {
+        edgeHead = name;
+    }
+    
+    public void setEdgeTail(String name) {
+        edgeTail = name;
+    }
+    
+    public void setTailDuration(int num) {
+        tailDuration = num;
+    }
+
+    public void setEdge(String head, String tail, int dur) {
+    	this.setEdgeHead(head);
+    	this.setEdgeTail(tail);
+    	this.setTailDuration(dur);
+    	
+    }
+
+    
+	
+	
+	
+}
---
+++ CSE360project-master/CSE360project-master/Path.java
@@ -0,0 +1,41 @@
+import java.util.Comparator;
+
+public class Path {
+	
+	private String pathNodes;
+	private int pathDuration;
+	
+	// Constructor
+    public Path() {
+        pathNodes = "";
+        pathDuration = 0;
+    }
+    
+    public String getPathNodes() {
+        return pathNodes;
+    }
+    
+    public int getPathDuration() {
+        return pathDuration;
+    }
+	
+    public void setPathNodes(String str) {
+    	pathNodes = str;
+    }
+    
+    public void setPathDuration(int num) {
+    	pathDuration = num;
+    }
+    
+    public static Comparator<Path> sortDur = new Comparator<Path>() {
+
+    	public int compare(Path p1,Path p2) {
+
+    	   int dur1 = p1.getPathDuration();
+    	   int dur2 = p2.getPathDuration();
+
+    	   /*For ascending order*/
+    	   return dur1-dur2;
+    	}
+    };
+}
