2d5e81b261c7747cd00fc33e84c0bf1c82526678
gkpalomo <43460233+gkpalomo@users.noreply.github.com>
2018-10-04 20:21:14 -0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,18 +1,28 @@
+
 import javax.swing.*;
+
+import java.awt.Container;
 import java.util.*;
 
+@SuppressWarnings("deprecation")
 public class main extends JApplet
 {
-    private int APPLET_WIDTH = 800, APPLET_HEIGHT = 600;
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int APPLET_WIDTH = 800, APPLET_HEIGHT = 600;
     private ProjectPanel projPanel;
     private Vector <Project> projectList;
 
     //The method init initializes the Applet
     public void init()
     {
-        projectList = new Vector<Project>();
+    	projectList = new Vector<Project>();
         projPanel = new ProjectPanel(projectList);
         getContentPane().add(projPanel);
         setSize (APPLET_WIDTH, APPLET_HEIGHT); //set Applet size
     }
+    
+   
 }  
