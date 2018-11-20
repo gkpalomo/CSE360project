2a12fcad7229bab7890a1fafdd985550794c1152
elin620 <43224346+elin620@users.noreply.github.com>
2018-11-19 18:16:32 -0700

    Add files via upload

---
+++ CSE360_1119/PathFinder.java
@@ -0,0 +1,24 @@
+
+import java.util.Vector;
+
+//import javax.swing.JApplet;
+import javax.swing.JFrame;
+
+public class PathFinder {
+
+	private static ProjectPanel projPanel;
+	private static Vector<Project> projectList;
+
+	public static void main(String[] arg) {
+		JFrame frame = new JFrame();
+		projectList = new Vector<Project>();
+        projPanel = new ProjectPanel(projectList);
+		frame.getContentPane().add(projPanel);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setSize(800, 700);
+		frame.setJMenuBar(projPanel.menuBar);
+		frame.setTitle("Activity Planner v3.6");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
